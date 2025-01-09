e041c12da11da6c574b775e3ac862015a31bbf3f
unknown <jamilafzal255@gmail.com>
2025-01-10 03:06:22 +0500

    classes in ts keyword(gettter,setter)

--- dist/classes.js
+++ dist/classes.js
@@ -1,10 +1,49 @@
 "use strict";
-class Information {
+// class Information {
+//   name: string
+//   email: string
+//   password: string = ''
+//   constructor(name: string, email: string) {
+//     this.name = name
+//     this.email = email
+//   }
+// }
+// const userinfo = new Information('jamil', 'jamil@gmail.com')
+class SuperHero {
     constructor(name, email) {
-        this.password = '';
         this.name = name;
         this.email = email;
     }
+    get heroEmail() {
+        //   getter is assocer the value in the class
+        return this.email;
+    }
+    set setHeroEmail(email) {
+        this.email = email;
+    }
 }
-const userinfo = new Information('jamil', 'jamil@gmail.com');
-console.log(userinfo);
+const hero1 = new SuperHero('jamil', 'jamil@gmail.com');
+// console.log(hero1.email)
+console.log((hero1.setHeroEmail = 'j@j.com'));
+class UserName {
+    constructor() {
+        this._name = 'jamil';
+        this._email = 'j@j.com';
+        this._password = '1122';
+        this.userName = 'coder..';
+    }
+    get userData() {
+        return {
+            userName: this.userName,
+            _name: this._name,
+            _email: this._email,
+            _password: this._password,
+        };
+    }
+    set passwordSet(_password) {
+        this._password = _password;
+    }
+}
+const datas = new UserName();
+console.log((datas.passwordSet = '112233'));
+console.log(datas.userData);
--- src/classes.ts
+++ src/classes.ts
@@ -1,12 +1,50 @@
-class Information {
-  name: string
-  email: string
-  password: string = ''
-  constructor(name: string, email: string) {
-    this.name = name
+// class Information {
+//   name: string
+//   email: string
+//   password: string = ''
+//   constructor(name: string, email: string) {
+//     this.name = name
+//     this.email = email
+//   }
+// }
+
+// const userinfo = new Information('jamil', 'jamil@gmail.com')
+
+class SuperHero {
+    constructor(public name: string, private email: string) {}
+  public get heroEmail() {
+    //   getter is assocer the value in the class
+    return this.email
+  }
+  public set setHeroEmail(email: string) {
     this.email = email
   }
 }
 
-const userinfo = new Information('jamil', 'jamil@gmail.com')
-console.log(userinfo)
+const hero1 = new SuperHero('jamil', 'jamil@gmail.com')
+// console.log(hero1.email)
+
+console.log((hero1.setHeroEmail = 'j@j.com'))
+
+class UserName {
+  private _name: string = 'jamil'
+  private _email: string = 'j@j.com'
+  private _password: string = '1122'
+  public userName: string = 'coder..'
+
+  get userData(): {} {
+    return {
+      userName: this.userName,
+      _name: this._name,
+      _email: this._email,
+      _password: this._password,
+    }
+  }
+  set passwordSet(_password: string) {
+    this._password = _password
+  }
+}
+
+const datas = new UserName()
+console.log((datas.passwordSet = '112233'))
+console.log(datas.userData)
